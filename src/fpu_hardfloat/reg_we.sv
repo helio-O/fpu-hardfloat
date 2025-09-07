8ddb53b6bb10580f3c5d55c4e038bea8ea10cd72
helio-O <79259450+helio-O@users.noreply.github.com>
2025-09-07 03:09:29 +0200

    Rename reg_we.sv to src/fpu_hardfloat/reg_we.sv

--- src/fpu_hardfloat/reg_we.sv
+++ src/fpu_hardfloat/reg_we.sv
@@ -10,7 +10,7 @@ always_ff @(posedge clk) begin
  if (!rst) begin
             data_out <= 0; // Reiniciar el registro a cero
         end else if (we) begin
-            data_out <= data_in; // Cargar datos de entrada si la señal de escritura está activa
+            data_out <= data_in; // Cargar datos de entrada si la seÃ±al de escritura estÃ¡ activa
         end
 end
 
